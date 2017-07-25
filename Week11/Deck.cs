e61aab185e41a896113e9d53fab43f63f0447943
Rosa <munguiarosa13@gmail.com>
2017-07-25 17:19:30 -0400

    Created deck class

---
+++ Week11/Deck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*Name:Rosa Munguia
+ * Date: July 25 2017
+ Description: This is the deck class
+ version0.1-Created deck class
+     */
+namespace Week11
+{
+
+    public class Deck: List<Card>//composing list of type card
+    {
+        //Private instance variables
+        //public properties
+        //Constrcutor
+        //Private methods
+        //Public methods
+    }
+}
